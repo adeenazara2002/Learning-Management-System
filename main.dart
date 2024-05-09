f1ff5adf916cd8866753523d82fbc320c97ca8cb
adeenazara2002 <adinazara2001@gmail.com>
2024-05-09 15:17:22 +0500

    Adding Login Functionality

--- main.dart
+++ main.dart
@@ -1,4 +1,33 @@
 import 'dart:io';
-void main(){
+// import 'teacher.dart';
+// import 'student.dart';
+// import 'admin.dart';
+import 'login.dart';
+
+void main(){ 
+  print("********LEARNING MANAGEMENT SYSTEM********");
   
+  while(true){
+    print("1. Login");
+    print("2. Sign up");
+    print("Enter Your choice");
+
+    var inp = int.parse(stdin.readLineSync()!);
+
+     if(inp == 1){
+      print("Enter Username");
+      var userName = (stdin.readLineSync()!);
+
+      print("Enter Password");
+      var pass = (stdin.readLineSync()!);
+
+      login(userName , pass);
+      break;
+
+  }
+
+  }
+ 
+
+
 }
